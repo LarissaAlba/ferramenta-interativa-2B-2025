856eaffd2e7899acf821bdafa8a29846dd5066a2
Larissa Alba <alba.santos.larissa@escola.pr.gov.br>
2025-10-01 15:47:54 -0300

    m.a.p.t.s

--- assets/app.js
+++ assets/app.js
@@ -5,18 +5,13 @@ function criaCartao(categoria, pergunta, resposta) {
 }
 //17-09-25//
 cartao.innerHTML = `
-        <!-- <article class="cartao">
-                <div class="cartao__conteudo">
-                        <h3>Programação</h3>
-                        <div class="cartao__conteudo__pergunta">
-                                <p>Qem é o homem que teve o poder de andar sobre o mar?</p>
-                        </div>
-                        <div class="cartao__conteudo__resposta">
-                                <p>O nome dEle é Jeus, seu nome é poder.</p>
-                        </div>
-                </div>
-        </article> -->
-
-        container.appendChild(cartao)
-
-        
+        <div class="cartao__conteudo">
+        <h3>${categoria}</h3>
+        <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+        </div>
+        </div>
+`
